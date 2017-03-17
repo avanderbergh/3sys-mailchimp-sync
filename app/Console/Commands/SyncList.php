6209787ea31401d1977f60429e23866527f65e34
Adriaan van der Bergh <avanderbergh@gmail.com>
2017-03-17 10:48:04 +0100

    Updated the query to only add the Main address for relationships rank 3 and below

--- app/Console/Commands/SyncList.php
+++ app/Console/Commands/SyncList.php
@@ -43,50 +43,49 @@ class SyncList extends Command
 
         $completed_emails = [];
         $wcbs = resolve('App\Services\WCBSApi');
-        $url = 'https://3sys.isdedu.de/WCBSAPI.ODataApi/CurrentPupil?$select=PupilPerson&$expand=PupilPerson($select=Relationships;$expand=Relationships($select=FromPupilPersonID,ToContactPersonID,RelationshipTypeTo,Rank;$expand=ToContactPerson($select=Title,FirstNames,Surname;$expand=EmailAddresses($select=EmailAddress))))&$filter=AcademicYearCode+eq+'.$year.' and InUse eq true';
+        $url = 'https://3sys.isdedu.de/WCBSAPI.ODataApi/CurrentPupil?$select=PupilPerson&$expand=PupilPerson($select=Relationships;$expand=Relationships($select=FromPupilPersonID,ToContactPersonID,RelationshipTypeTo,Rank;$expand=ToContactPerson($select=Title,FirstNames,Surname;$expand=EmailAddresses($select=EmailAddress,EmailAddressTypeDescription))))&$filter=AcademicYearCode+eq+'.$year.'+and+InUse+eq+true';
         $result = $wcbs->request($url); // Fetch the data from 3Sys through the API
         $this->info('Syncing List');
         foreach ($result->value as $value) {
             foreach ($value->PupilPerson->Relationships as $relationship) {
-                foreach ($relationship->ToContactPerson->EmailAddresses as $emailAddress) {
+                if ($relationship->Rank <= 3){
+                    foreach ($relationship->ToContactPerson->EmailAddresses as $emailAddress) {
 //                  Check if this address has already been updated in this run
-                    if (!in_array($emailAddress->EmailAddress, $completed_emails)) {
-                        try {
+                        if ($emailAddress->EmailAddressTypeDescription == 'Main' && !in_array($emailAddress->EmailAddress,$completed_emails)) {
+                            try {
 //                            First try to update an existing address, if this fails a new address will be created.
-                            $this->info('Attempting to update '.$emailAddress->EmailAddress);
-                            MC::patch('lists/'.$listID.'/members/'.md5(strtolower($emailAddress->EmailAddress)), [
-                                'email_address' => $emailAddress->EmailAddress,
-                                'merge_fields' => [
-                                    'FNAME' => $relationship->ToContactPerson->FirstNames,
-                                    'LNAME' => $relationship->ToContactPerson->Surname,
-                                    'TITLE' => $relationship->ToContactPerson->Title
-                                ]
-                            ]);
-                            $this->info('Updated '.$emailAddress->EmailAddress);
-                            $completed_emails[] = $emailAddress->EmailAddress;
-                        } catch (\Exception $e){
-                            try{
-//                                Create a new member
-                                $this->info('Attempting to create '.$emailAddress->EmailAddress);
-                                MC::post('lists/'.$listID.'/members', [
+                                $this->info('Attempting to update '.$emailAddress->EmailAddress);
+                                MC::patch('lists/'.$listID.'/members/'.md5(strtolower($emailAddress->EmailAddress)), [
                                     'email_address' => $emailAddress->EmailAddress,
-                                    'status' => 'subscribed',
                                     'merge_fields' => [
                                         'FNAME' => $relationship->ToContactPerson->FirstNames,
                                         'LNAME' => $relationship->ToContactPerson->Surname,
                                         'TITLE' => $relationship->ToContactPerson->Title
                                     ]
                                 ]);
-                                $this->info('Created '.$emailAddress->EmailAddress);
+                                $this->info('Updated '.$emailAddress->EmailAddress);
                                 $completed_emails[] = $emailAddress->EmailAddress;
-                            } catch (\Exception $f) {
+                            } catch (\Exception $e){
+                                try{
+//                                Create a new member
+                                    $this->info('Attempting to create '.$emailAddress->EmailAddress);
+                                    MC::post('lists/'.$listID.'/members', [
+                                        'email_address' => $emailAddress->EmailAddress,
+                                        'status' => 'subscribed',
+                                        'merge_fields' => [
+                                            'FNAME' => $relationship->ToContactPerson->FirstNames,
+                                            'LNAME' => $relationship->ToContactPerson->Surname,
+                                            'TITLE' => $relationship->ToContactPerson->Title
+                                        ]
+                                    ]);
+                                    $this->info('Created '.$emailAddress->EmailAddress);
+                                    $completed_emails[] = $emailAddress->EmailAddress;
+                                } catch (\Exception $f) {
 //                                Creating has failed for some reason, show an error message
-                                $this->error($f->getMessage());
+                                    $this->error($f->getMessage());
+                                }
                             }
                         }
-                    } else {
-//                        Show a message that this address was already updated.
-                        $this->info($emailAddress->EmailAddress.' has been updated already');
                     }
                 }
             }
