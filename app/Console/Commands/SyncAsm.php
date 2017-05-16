e54862cceb7d48588a0aa10ae7095ecac7312f3e
Adriaan van der Bergh <avanderbergh@gmail.com>
2017-05-16 14:19:27 +0200

    Updated exclusion list

--- app/Console/Commands/SyncAsm.php
+++ app/Console/Commands/SyncAsm.php
@@ -38,7 +38,7 @@ class SyncAsm extends Command
      */
     public function handle()
     {
-        $exclude_subject_sets = ['1928206877', '1928206878', '1928206879', '1928207281', '1931718732', '1928207026'];
+        $exclude_subject_sets = ['1928206877', '1928206878', '1928206879', '1928207281', '1931718732', '1928207026', '1936469362'];
 
         $year = $this->argument('year');    // the academic year
         $wcbs = resolve('App\Services\WCBSApi');
